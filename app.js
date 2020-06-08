9749fdfebe338df0c5eb3a0a09b69b8a97570c61
Nirali Parekh <nirali25parekh@gmail.com>
2020-06-09 01:08:30 +0530

    graphql basic eg 1

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const graphQlHttp = require('express-graphql') //-> returns a function
+const { buildSchema } = require ('graphql') // returns object and we destructure it
 
 const app = express()
 
-app.get('/', (req,res,next) => {
-    res.send('Hello')
-})
+app.use(
+    '/graphql',
+ graphQlHttp({      //params:  schemas, resolvers necessary
+
+     //schema     
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery,
+            mutation: RootMutation,
+        }
+    `), 
+
+    //  resolvers
+    rootValue: {
+        events: () => { // when 'events' property triggered, this function will fire off
+            return ['1', '2', '3', '4']
+        },
+        createEvent : (args) => {   // when 'createEvents' property triggered, this function will fire off
+            return args.name
+        }
+    },   
+    
+    //for GUI
+    graphiql: true,     
+}))
 app.listen(3000)
