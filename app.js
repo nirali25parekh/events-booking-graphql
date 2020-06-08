5f38ffbe4a3ec71279991d882f5c09f58e68e8a5
Nirali Parekh <nirali25parekh@gmail.com>
2020-06-09 01:22:33 +0530

    graphql basic eg 2

--- app.js
+++ app.js
@@ -5,18 +5,36 @@ const { buildSchema } = require ('graphql') // returns object and we destructure
 
 const app = express()
 
+var events = []
+
 app.use(
     '/graphql',
  graphQlHttp({      //params:  schemas, resolvers necessary
 
      //schema     
     schema: buildSchema(`
+        type Event {
+            _id : ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
+        }
+
+        input EventInput {
+            _id : ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -28,10 +46,18 @@ app.use(
     //  resolvers
     rootValue: {
         events: () => { // when 'events' property triggered, this function will fire off
-            return ['1', '2', '3', '4']
+            return events
         },
         createEvent : (args) => {   // when 'createEvents' property triggered, this function will fire off
-            return args.name
+            const event = {
+                _id : args.eventInput._id,
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                date: args.eventInput.date,
+                price: args.eventInput.price,
+            }
+            events.push(event)
+            return event
         }
     },   
     
